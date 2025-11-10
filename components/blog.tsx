5a6dc0949c80f96d720597a91a77bfa7a7e9483d
Sammymarigamukami <marigasam18@gmailcom>
2025-11-10 21:10:14 +0300

    update

--- components/blog.tsx
+++ components/blog.tsx
@@ -17,10 +17,6 @@ export default function BlogsPage() {
     speed: 500,
     responsive: [
       {
-        breakpoint: 10000,
-        settings: { slidesToShow: 3, slidesToScroll: 1 }
-      },
-      {
         breakpoint: 1024,
         settings: { slidesToShow: 2, slidesToScroll: 1 }
       },
