3d3effc4c8ef32d01d886049621abea83bd2046d
Sammymarigamukami <marigasam18@gmailcom>
2025-11-10 15:37:01 +0300

    update

--- components/blog.tsx
+++ components/blog.tsx
@@ -11,13 +11,18 @@ export default function BlogsPage() {
     focusOnSelect: true,
     dots: true,
     autoplay: true,
+    slidesToShow: 3,
     infinite: true,
     slidesToScroll: 1,
     speed: 500,
     responsive: [
       {
+        breakpoint: 10000,
+        settings: { slidesToShow: 3, slidesToScroll: 1 }
+      },
+      {
         breakpoint: 1024,
-        settings: { slidesToShow: 2, slideToScroll: 1 }
+        settings: { slidesToShow: 2, slidesToScroll: 1 }
       },
       {
         breakpoint: 768,
