2f6ca5d33dc4b9498f867db48fdcaa50ad5fd57d
Sammymarigamukami <marigasam18@gmailcom>
2025-11-13 18:39:37 +0300

    update

---
+++ app/sitemap.ts
@@ -0,0 +1,48 @@
+import { products } from "@/lib/data"
+
+
+export default async function sitemap() {
+
+    const product = products.slice(0, 10);
+
+    const productUrls = product.map((product) => ({
+        url: `${process.env.BASE_URL}/products/${product.category?.toLowerCase()}/${product.name}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 0.7,
+    }));
+
+
+    return [
+        {
+            url: `${process.env.BASE_URL}/`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1.0,
+        },
+        {
+            url: `${process.env.BASE_URL}/blogs`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.BASE_URL}/products`,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.BASE_URL}/contact`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.5,
+        },
+        {
+            url: productUrls,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.7,
+        }
+    ]
+}
