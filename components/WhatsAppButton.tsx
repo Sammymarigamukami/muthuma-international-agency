bdd320b708526e6da55fe4d8bd31fa2f944a481b
Sammymarigamukami <marigasam18@gmailcom>
2025-11-17 16:05:32 +0300

    update

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -6,7 +6,7 @@ import { FaWhatsapp } from "react-icons/fa6";
 export default function WhatsAppButton() {
     return (
         <Link
-        href="https://wa.me/254715907311"
+        href="https://wa.me/254790921006"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-all duration-300 z-50"
